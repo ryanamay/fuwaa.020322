0a1ce8bc3df07f0a5706ce1ff8a70e51425274cc
nafunii <kawaii@duck.com>
2022-01-30 15:36:28 +0800

    cleanup

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,7 @@
-import Image from 'next/image';
+
 import SEO from '../components/SEO';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
-import Content from '../components/About';
 
 export default function Blog() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
-import Image from 'next/image';
 import SEO from '../components/SEO';
-import Banner from '../components/Banner';
-import FadeIn from 'react-fade-in';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -9,31 +6,9 @@ import Footer from '../components/Footer';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import Experience from '../components/Experience';
 import { useEffect } from 'react';
 
 export default function Home() {
-
-useEffect(() => {
-    function reveal() {
-      var reveals = document.querySelectorAll(".reveal");
-    
-      for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-    
-        if (elementTop < windowHeight - elementVisible) {
-          reveals[i].classList.add("active");
-        } else {
-          reveals[i].classList.remove("active");
-        }
-      }
-    }
-    
-    window.addEventListener("scroll", reveal);
-
-})
   return (
     <main className="font-Manrope relative">
         <SEO
